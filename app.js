da7c9ff15242717a0e5a66ce18b5f9e910d2dc6b
koriscs <koriscs@gmail.com>
2022-06-02 17:16:47 +0200

    Changed process.env.PORT andPGPORT

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 
 dotenv.config();
 
-const port = process.env.PGPORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) =>{
     res.send("<h1>We are lit2!</h1>");
