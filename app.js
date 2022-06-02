ee9942f6063d6fe633e34872e00b7d2cc28ebf12
koriscs <koriscs@gmail.com>
2022-06-02 18:18:57 +0200

    trying with heroku to work

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.get('/', (req, res) =>{
     
     pool.query('SELECT * FROM customers;',(error, results) =>{
         if (error) throw error;
-        res.status(200).json(results.rows);
+        res.send(results.rows);
     })
     
 })
